3acacd7c567d69e61bd54b2633a74943e1409c73
Capillary J <jason9075@gmail.com>
2014-05-05 23:20:53 +0800

    new thumb

--- app/js/main.js
+++ app/js/main.js
@@ -9,7 +9,7 @@
  * Copyright 2014, Script Tutorials
  * http://www.script-tutorials.com/
  */
-
+var i= 1;
 $(document).ready(function (){
 
   /* Every time the window is scrolled ... */
@@ -20,35 +20,27 @@ $(document).ready(function (){
         /* Check the location of each desired element */
         $('.hideme').each( function(i){
             
-            bottom_of_object = $(this).position().top + $(this).outerHeight();
+            bottom_of_object = $(this).offset().top + $(this).height();
             bottom_of_window = $(window).scrollTop() + $(window).height();
             
             /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object + 800){
-                $(this).animate({'opacity':'1','margin':'0'},1000)
+            if( bottom_of_window > bottom_of_object ){
+                // $(this).animate({'opacity':'1','margin':'0'},1000)
+                $(this).addClass("slide");
             }
         }); 
+
+        
 
         /* Check the location of each desired element */
         $('.progress .progress-bar').each( function(i){
             
-            bottom_of_object = $(this).position().top + $(this).outerHeight();
+            bottom_of_object = $(this).offset().top + $(this).outerHeight();
             bottom_of_window = $(window).scrollTop() + $(window).height();
             
             /* If the object is completely visible in the window, increasing it it */
-            if( bottom_of_window > bottom_of_object +1550 ){
+            if( bottom_of_window > bottom_of_object){
                 $(this).css({'-webkit-animation-name':'increasing','-moz-animation-name': 'increasing', 'opacity':'1'})
-            }
-        });
-
-        $('.thumbnail').each( function(i){
-            
-            bottom_of_object = $(this).position().top + $(this).outerHeight();
-            bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* If the object is completely visible in the window, increasing it it */
-            if( bottom_of_window > bottom_of_object +2050 ){
-                $(this).css({'-webkit-animation-name':'toLarge','-moz-animation-name': 'toLarge', 'opacity':'0.9'})
             }
         });
     });
